f3c00c4f138500c6286349656c4cc930e6b56004
Andriy Shostak <andrew.shostak11@gmail.com>
2026-06-24 22:59:02 +0200

    Create migration plan, add `providers` table and `trigger_set_updated_at` function for automatic timestamp updates

---
+++ database/migrations/20260624223000_create_updated_at_trigger.up.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+CREATE OR REPLACE FUNCTION trigger_set_updated_at()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        NEW.updated_at = NOW();
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+
+COMMIT;
